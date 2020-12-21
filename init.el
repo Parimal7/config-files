1e0e5d0ab152c6666eb3b96f4730cfe4bfe0bb6c
Parimal <parimalprasoon7@gmail.com>
2020-12-21 08:27:03 +0530

    added updated dot files

--- init.el
+++ init.el
@@ -23,13 +23,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(hl-todo-keyword-faces
+   '(("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f")))
  '(org-agenda-files '("~/Github/org-mode/agenda-files/agenda.org"))
  '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(org-journal linum-relative htmlize company-irony company-c-headers flycheck-clang-analyzer flycheck popup-kill-ring spaceline company dashboard rainbow-delimiters hungry-delete sudo-edit rainbow-mode neotree magit google-this org-bullets spacemacs-theme which-key use-package)))
+   '(org-journal linum-relative htmlize company-irony company-c-headers flycheck-clang-analyzer flycheck popup-kill-ring spaceline company dashboard rainbow-delimiters hungry-delete sudo-edit rainbow-mode neotree magit google-this org-bullets spacemacs-theme which-key use-package))
+ '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
